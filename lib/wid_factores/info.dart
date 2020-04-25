d65f47d11fe3496bb5e5a73d6172df22d0b6ab5a
daniort <dqniort@gmail.com>
2020-04-25 03:02:55 -0500

    quedo mas chido Info

--- lib/wid_factores/info.dart
+++ lib/wid_factores/info.dart
@@ -12,20 +12,17 @@ class Info extends StatefulWidget {
 class _InfoState extends State<Info> {
   @override
   Widget build(BuildContext context) {
-    
-   
-
-  
     return Scaffold(
-      backgroundColor: Color(0xff19535f),
+      //backgroundColor: Color(0xff19535f),
+      backgroundColor: Color(0xffdadad9),
       appBar: AppBar(
         title: Text('Mas Información'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: SingleChildScrollView(
-                  child: Container(
-            height: ((MediaQuery.of(context).size.height)),
+          child: Container(
+            height: ((MediaQuery.of(context).size.height) * 1.4),
             width: ((MediaQuery.of(context).size.width)),
             decoration: BoxDecoration(
               color: Color(0xfff0f3f5),
@@ -34,116 +31,173 @@ class _InfoState extends State<Info> {
             child: Column(
               children: <Widget>[
                 Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
+                   padding: const EdgeInsets.only(
+                      top: 70.0, left: 10.0, right: 10.0, bottom: 10.0),
+                  child: Container(
+                    width: ((MediaQuery.of(context).size.width) * 0.4),
+                    child: Image.asset("lib/assets/images/logotrans.png"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 5.0, left: 10.0, right: 10.0, bottom: 10.0),
+                  child: Center(
+                    child: Text(
+                        'Herramienta de orientación sanitaria, recomendaciones y auto diagnóstico personal para COVID-2019.',
+                        textAlign: TextAlign.center,
+                        style:
+                            TextStyle(color: Colors.blueGrey, fontSize: 16.0)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0),
                   child: Container(
                     height: ((MediaQuery.of(context).size.height) * 0.05),
                     child: Center(
                       child: Text('Desarrollada por:',
-                          style: GoogleFonts.doHyeon(
-                              color: Colors.blueGrey, fontSize: 18.0)),
+                          style: TextStyle(
+                              color: Colors.blueGrey, fontSize: 15.0)),
                     ),
                   ),
                 ),
                 Container(
-                  height: ((MediaQuery.of(context).size.height) * 0.2),
-                  child: Image.asset("lib/assets/images/upsz-1-1.jpg"),
+                  width: ((MediaQuery.of(context).size.width) * 0.7),
+                  child: Image.asset("lib/assets/images/logocoz.png"),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 12.0),
+                  padding: const EdgeInsets.only(top:8.0),
+                  child: Container(
+                    width: ((MediaQuery.of(context).size.width) * 0.7),
+                    child: Image.asset("lib/assets/images/logolabsol.png"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 5.0),
                   child: Container(
                     height: ((MediaQuery.of(context).size.height) * 0.05),
                     child: Center(
                       child: Text('Bajo Licencia GPL 3.0',
-                          style: GoogleFonts.doHyeon(
-                              color: Color(0xff19535f), fontSize: 18.0)),
+                          style: TextStyle(
+                              color: Color(0xff19535f), fontSize: 15.0)),
                     ),
                   ),
                 ),
-                 Padding(
-                  padding: const EdgeInsets.only(top:10.0,bottom: 5.0),
-                   child: Center(
-                      child: Text('Project Manager:',
-                          style: GoogleFonts.doHyeon(
-                              color:  Colors.blueGrey, fontSize: 14.0)),
-                    ),
-                
+                Padding(
+                   padding: const EdgeInsets.only(
+                      top: 70.0, left: 10.0, right: 10.0, bottom: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 2,
+                        child: Center(
+                            child: Text(
+                          'Project Manager',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        )),
+                      ),
+                    ],
+                  ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(1.0),
-                   child: Center(
-                      child: Text('Manuel Haro',
-                          style: GoogleFonts.doHyeon(
-                              color:  Colors.blueGrey, fontSize: 18.0)),
-                    ),
-                
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  padding: const EdgeInsets.all(4.0),
                   child: Center(
-                    child: Text('Desarrolladores:',
-                        style: GoogleFonts.doHyeon(
-                            color: Colors.blueGrey, fontSize: 14.0)),
+                    child: Text('Manuel Haro',
+                        style:
+                            TextStyle(color: Colors.blueGrey, fontSize: 15.0)),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(1.0),
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 2,
+                        child: Center(
+                            child: Text(
+                          'Desarrolladores',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        )),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
                   child: Center(
                     child: Text('Juan José Pelaez Villegas',
-                        style: GoogleFonts.doHyeon(
-                            color: Colors.blueGrey, fontSize: 18.0)),
+                        style:
+                            TextStyle(color: Colors.blueGrey, fontSize: 15.0)),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(1.0),
+                  padding: const EdgeInsets.all(4.0),
                   child: Center(
                     child: Text('Daniel Ortiz Castro',
-                        style: GoogleFonts.doHyeon(
-                            color: Colors.blueGrey, fontSize: 18.0)),
+                        style:
+                            TextStyle(color: Colors.blueGrey, fontSize: 15.0)),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Center(
-                    child: Text('Asesores Médicos:',
-                        style: GoogleFonts.doHyeon(
-                            color: Colors.blueGrey, fontSize: 14.0)),
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 2,
+                        child: Center(
+                            child: Text(
+                          'Asesores Médicos',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        )),
+                      ),
+                    ],
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(1.0),
+                  padding: const EdgeInsets.all(4.0),
                   child: Center(
                     child: Text('Sandy Sierra',
-                        style: GoogleFonts.doHyeon(
-                            color: Colors.blueGrey, fontSize: 18.0)),
+                        style:
+                            TextStyle(color: Colors.blueGrey, fontSize: 15.0)),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(1.0),
+                  padding: const EdgeInsets.all(4.0),
                   child: Center(
                     child: Text('Manuel Escobar',
-                        style: GoogleFonts.doHyeon(
-                            color: Colors.blueGrey, fontSize: 18.0)),
+                        style:
+                            TextStyle(color: Colors.blueGrey, fontSize: 15.0)),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(1.0),
+                  padding: const EdgeInsets.all(4.0),
                   child: Center(
                     child: Text('Eduardo Gamez',
-                        style: GoogleFonts.doHyeon(
-                            color: Colors.blueGrey, fontSize: 18.0)),
+                        style:
+                            TextStyle(color: Colors.blueGrey, fontSize: 15.0)),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(1.0),
-                   child: Center(
-                      child: Text('Bruno Reimers',
-                          style: GoogleFonts.doHyeon(
-                              color:  Colors.blueGrey, fontSize: 18.0)),
-                    ),
-                
+                  padding: const EdgeInsets.all(4.0),
+                  child: Center(
+                    child: Text('Bruno Reimers',
+                        style:
+                            TextStyle(color: Colors.blueGrey, fontSize: 15.0)),
+                  ),
                 ),
-
-
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Center(
+                    child: Text('José Casillas',
+                        style:
+                            TextStyle(color: Colors.blueGrey, fontSize: 15.0)),
+                  ),
+                ),
               ],
             ),
           ),
